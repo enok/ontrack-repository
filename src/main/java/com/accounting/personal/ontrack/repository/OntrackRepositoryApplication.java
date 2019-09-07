fa256b17f04d9ff03b9668115ad14e2fc33d2f97
Enok <enok.jesus@gmail.com>
2019-09-06 23:01:17 -0300

    first commit

---
+++ src/main/java/com/accounting/personal/ontrack/repository/OntrackRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.accounting.personal.ontrack.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntrackRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OntrackRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accounting/personal/ontrack/repository/OntrackRepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accounting.personal.ontrack.repository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OntrackRepositoryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
